d1ac3ee73eb546c7a0767bb7fe0d92e2d459e720
Sanaa El Fakiri <elfakiri.sanaa@gmail.com>
2022-05-23 23:44:38 +0100

    fix responsive

--- src/components/resume.js
+++ src/components/resume.js
@@ -6,18 +6,18 @@ class Resume extends Component {
     render() {
         return(
             <div className="container w-75">
-                <div className="row mb-5 mt-5">
-                    <div className="col-3 pe-5">
+                <div className="row mb-md-5 mt-5">
+                    <div className="col-md-3 pe-5 pt-5">
                         <img src={profile} alt="Sanaa El Fakiri" className="img-fluid rounded-circle"/>
                     </div>
-                    <div className="col-9 mb-5">
+                    <div className="col-md-9 mb-5 pt-5">
                         <h1>Sanaa El Fakiri</h1>
                         <h5 className="fw-light text-black-50">Full-Stack Developer from Casablanca, Morocco</h5>
                         <div className="row mt-4">
-                            <div className="col-1">
+                            <div className="col-md-1">
                                 <a href="https://www.linkedin.com/in/sanaa-el-fakiri/" className="link-secondary text-decoration-none">LinkedIn</a>
                             </div>
-                            <div className="col-1 ms-3">
+                            <div className="col-md-1 ms-md-3">
                                 <a href="https://twitter.com/SanaaElfakiri" className="link-secondary text-decoration-none">Twitter</a>
                             </div>
                             
@@ -38,10 +38,10 @@ const Section = ({sections}) => {
         sections.map((section) => {
         return (
             <div className="row mb-5">
-                <div className="col-3 text-muted text-uppercase font-monospace">
+                <div className="col-md-3 text-muted text-uppercase font-monospace">
                     <h6>{section.name}</h6>
                 </div>
-                <div className="col-9">
+                <div className="col-md-9">
                     <SectionElements elements={section.elements}/>
                 </div>
             </div>
