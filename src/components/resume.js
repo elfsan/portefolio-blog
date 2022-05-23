34745a0c132c208acc70a79b1138989a4830ebcc
Sanaa El Fakiri <elfakiri.sanaa@gmail.com>
2022-05-23 23:10:28 +0100

    resume details

---
+++ src/components/resume.js
@@ -0,0 +1,72 @@
+import { Component } from "react";
+import { sections } from "../content/resumeData.js"; 
+import profile from '../profile-sanaa.jpeg'; 
+
+class Resume extends Component {
+    render() {
+        return(
+            <div className="container w-75">
+                <div className="row mb-5 mt-5">
+                    <div className="col-3 pe-5">
+                        <img src={profile} alt="Sanaa El Fakiri" className="img-fluid rounded-circle"/>
+                    </div>
+                    <div className="col-9 mb-5">
+                        <h1>Sanaa El Fakiri</h1>
+                        <h5 className="fw-light text-black-50">Full-Stack Developer from Casablanca, Morocco</h5>
+                        <div className="row mt-4">
+                            <div className="col-1">
+                                <a href="https://www.linkedin.com/in/sanaa-el-fakiri/" className="link-secondary text-decoration-none">LinkedIn</a>
+                            </div>
+                            <div className="col-1 ms-3">
+                                <a href="https://twitter.com/SanaaElfakiri" className="link-secondary text-decoration-none">Twitter</a>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+                <Section sections={sections}/>
+                <div className="row">
+                    <p className="text-muted font-monospace text-center small">elfakiri.sanaa@gmail.com</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+const Section = ({sections}) => {
+    return(
+        sections.map((section) => {
+        return (
+            <div className="row mb-5">
+                <div className="col-3 text-muted text-uppercase font-monospace">
+                    <h6>{section.name}</h6>
+                </div>
+                <div className="col-9">
+                    <SectionElements elements={section.elements}/>
+                </div>
+            </div>
+        );
+        })
+    );
+}
+
+const SectionElements = ({elements}) => {
+    return(
+        
+        elements.map((element) => {
+            let link = <></> ;
+            if (element.url) {
+                link = <a href={element.url} className="link-secondary text-decoration-none">More</a> ;
+            }
+            return(
+                <div className="row mb-2">
+                    <h6 className="lead"><a href={element.url} className="link-secondary text-decoration-none">{element.name}</a></h6>
+                    <h6>{element.title}</h6>
+                    <p className="text-black-50">{element.description}</p>
+                </div>
+            );
+        })
+    );
+}
+
+export default Resume;
---
+++ src/content/resumeData.js
@@ -0,0 +1,115 @@
+export const sections = [
+    {
+        'name': 'Experience',
+        'elements' : [
+            {
+                'name' : 'Sofrecom C2S | March 2019 - now',
+                'title' : 'Full Stack Web Developer',
+                'description' : 'I work as a  full-stack web developer. I am part of a team that works on the evolution, fixing and integration of new functionalities for a Orange France web application as well as the backoffice. For that I use Symfony, PHP, Smarty Template Engine, Javascript & libraries, HTML/CSS, Robot Selenium (Python), Salesforce CRM, Magic CMS.'
+            },
+            {
+                'name' : '',
+                'title' : 'Scrum Master',
+                'description' : 'I worked as a Scrum Master twice for the project above, my missions were the follow-up of the User Stories  and sub-tasks for a smooth running of the Scrum method and planning and leading of agile ceremonies (Daily Scrum, Retrospective, Review..)'
+            },
+        ]
+    },
+    {
+        'name': 'Education',
+        'elements' : [
+            {
+                'name' : 'ENSIAS | 2020 - 2022',
+                'title' : 'Data Science & Big Data Master\'s degree',
+                'description' : ''
+            },
+            {
+                'name' : 'IAV Hassan II | 2017 - 2019',
+                'title' : 'Space Science & Technologies - GNSS Master\'s degree',
+                'description' : ''
+            },
+            {
+                'name' : 'FST | 2013 - 2017',
+                'title' : 'Computer Engineering Bachelor degree',
+                'description' : ''
+            }
+        ]
+    },
+    {
+        'name': 'skills',
+        'elements' : [
+            {
+                'name' : 'Languages & Frameworks',
+                'title' : 'Programming',
+                'description' : 'PHP, Symfony, Python, Javascript, ReactJs HTML/CSS'
+            },
+            {
+                'name' : '',
+                'title' : 'Databases',
+                'description' : 'Mysql, MongoDb, SQLite'
+            },
+            {
+                'name' : '',
+                'title' : 'Data Science',
+                'description' : 'CNN, KNN, Decision Trees, Random Forests,SVM, Reinforcement learning avec TF, keras, Pytorch, SciKit, Pandas, NumPy'
+            },
+            {
+                'name' : '',
+                'title' : 'Other',
+                'description' : 'Git, Docker, Jira, Pentaho Data Integration, ArcGIS, Qgis'
+            }
+        ]
+    },
+    {
+        'name': 'activities',
+        'elements' : [
+            {
+                'name' : 'Women Techmakers',
+                'title' : 'Ambassador',
+                'description' : 'Events, speaking,  mentoring',
+                'url' : 'https://twitter.com/womentechmakers'
+            },
+            {
+                'name' : 'Google Developers Group Rabat',
+                'title' : 'Organizer',
+                'description' : 'Events organization, technical presentations, team management',
+                'url' : 'https://gdg.community.dev/gdg-rabat/'
+            },
+            {
+                'name' : 'Google Developers Group Casablanca',
+                'title' : 'Organizer',
+                'description' : 'Events organization',
+                'url' : 'https://gdg.community.dev/gdg-casablanca/',
+                'more' : 'https://gdgcasablanca.com'
+            },
+            {
+                'name' : 'Future Vision Association',
+                'title' : 'Team Lead',
+                'description' : 'Leading a team of 5 people (trainees) who developed showcase and E-commerce web applications (Stack: ReactJs, Django, gitlab-ci) for the benefit of local foundations',
+                'url' : 'https://www.fva.ma/'
+            },
+            {
+                'name' : 'Google Developer Students Club',
+                'title' : 'Advisor',
+                'description' : 'Mentoring GDSC Ensa Tangier members (students), Technical presentations',
+                'url' : 'https://developers.google.com/community/gdsc'
+            },
+            
+        ]
+    },
+    {
+        'name': 'Certifs',
+        'elements': [
+            {
+                'name': 'IBM',
+                'title': 'Big Data Engineer - Mastery Award 2021',
+                'url': 'https://www.credly.com/badges/c70bce5e-556d-4eed-b756-9c16e08ffbf2/public_url'
+            },
+            {
+                'name': 'Google',
+                'title': 'Data Analysis Certificate',
+                'description': '',
+                'url': ''
+            }
+        ]
+    }
+];
